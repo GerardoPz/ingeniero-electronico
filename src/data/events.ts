8fbdd90a4ae199068e285fde759e847ff982cb41
GerardoPz <pliegolugohectorgerardo@gmail.com>
2026-03-11 21:05:36 -0600

    Publicando fotos ruta

--- src/data/events.ts
+++ src/data/events.ts
@@ -40,7 +40,7 @@ export const pastEvents: PastEvent[] = [
     place: 'TecNM Centro Computo',
     description: 'Participación en hackathon con proyecto sobre cultura hidrica.',
     images: [
-      './public/img/hackatec.jpg',
+      '/ingeniero-electronico/img/hackatec.jpg',
     ],
   },
   {
@@ -50,13 +50,13 @@ export const pastEvents: PastEvent[] = [
     place: 'COLEGIO AMERICANO DE XALAPA',
     description: 'Stand y demostraciones de proyectos de las tres ingenierías.',
     images: [
-      './public/img/ExpoXalapa/eq-exp.jpg',
-      './public/img/ExpoXalapa/acreditacion-expx.jpg',
-      './public/img/ExpoXalapa/eq-exp2.jpg',
-      './public/img/ExpoXalapa/eq-exp3.JPG',
-      './public/img/ExpoXalapa/expxalapa.jpg',
-      './public/img/ExpoXalapa/expoxalapa2.jpg',
-      './public/img/ExpoXalapa/expoxalapa3.JPG',
+      '/ingeniero-electronico/img/ExpoXalapa/eq-exp.jpg',
+      '/ingeniero-electronico/img/ExpoXalapa/acreditacion-expx.jpg',
+      '/ingeniero-electronico/img/ExpoXalapa/eq-exp2.jpg',
+      '/ingeniero-electronico/img/ExpoXalapa/eq-exp3.JPG',
+      '/ingeniero-electronico/img/ExpoXalapa/expxalapa.jpg',
+      '/ingeniero-electronico/img/ExpoXalapa/expoxalapa2.jpg',
+      '/ingeniero-electronico/img/ExpoXalapa/expoxalapa3.JPG',
     ],
   },
   {
@@ -66,17 +66,16 @@ export const pastEvents: PastEvent[] = [
     place: 'Tampico, Tamaulipas',
     description: 'Asistencia a conferencia sobre transformación digital.',
     images: [
-      './public/img/ExpoTampico/reconocimientotampico.jpg',
-      './public/img/ExpoTampico/expotampico.JPG',
-      './public/img/ExpoTampico/expotampico2.jpg',
-      './public/img/ExpoTampico/expotampico3.jpg',
-      './public/img/ExpoTampico/expotampico4.jpg',
-      './public/img/ExpoTampico/expotampico5.jpg',
-      './public/img/ExpoTampico/expotampico6.jpg',
-      './public/img/ExpoTampico/expotampico7.jpg',
-      './public/img/ExpoTampico/expotampico8.jpg',
-      './public/img/ExpoTampico/expotampico9.jpg',
-
+      '/ingeniero-electronico/img/ExpoTampico/reconocimientotampico.jpg',
+      '/ingeniero-electronico/img/ExpoTampico/expotampico.JPG',
+      '/ingeniero-electronico/img/ExpoTampico/expotampico2.jpg',
+      '/ingeniero-electronico/img/ExpoTampico/expotampico3.jpg',
+      '/ingeniero-electronico/img/ExpoTampico/expotampico4.jpg',
+      '/ingeniero-electronico/img/ExpoTampico/expotampico5.jpg',
+      '/ingeniero-electronico/img/ExpoTampico/expotampico6.jpg',
+      '/ingeniero-electronico/img/ExpoTampico/expotampico7.jpg',
+      '/ingeniero-electronico/img/ExpoTampico/expotampico8.jpg',
+      '/ingeniero-electronico/img/ExpoTampico/expotampico9.jpg',
     ],
   },
   {
@@ -85,22 +84,19 @@ export const pastEvents: PastEvent[] = [
     date: '2025',
     place: 'CITCA del CENIDET',
     description: 'Participación en publicación en revista de CITCA del CENIDET.',
-    images: [
-
-    ],
+    images: [],
   },
   {
-    id: 'past4',
+    id: 'past5', // Corregido el ID que estaba repetido como past4
     title: 'Encuentro Nacional de Robotica 2026',
     date: 'Febrero 2026',
     place: 'Tlalpan, Ciudad de México',
     description: 'Participación en encuentro nacional de robotica con proyecto "ORI-BOT".',
     images: [
-      './public/img/EncuentroNacionalRobotica/robotica.jpg',
-      './public/img/EncuentroNacionalRobotica/robotica2.JPG',
-      './public/img/EncuentroNacionalRobotica/robotica3.jpg',
-      './public/img/EncuentroNacionalRobotica/robotica4.jpg',
-
+      '/ingeniero-electronico/img/EncuentroNacionalRobotica/robotica.jpg',
+      '/ingeniero-electronico/img/EncuentroNacionalRobotica/robotica2.JPG',
+      '/ingeniero-electronico/img/EncuentroNacionalRobotica/robotica3.jpg',
+      '/ingeniero-electronico/img/EncuentroNacionalRobotica/robotica4.jpg',
     ],
   },
 ];
